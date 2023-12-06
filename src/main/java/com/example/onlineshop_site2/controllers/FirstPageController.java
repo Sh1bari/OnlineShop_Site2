cad8b7b547de6f89c6d7d584b33853be30ce820b
Sh1bari <vova_krasnov_2004@mail.ru>
2023-12-06 15:07:46 +0300

    new

--- src/main/java/com/example/onlineshop_site2/controllers/FirstPageController.java
+++ src/main/java/com/example/onlineshop_site2/controllers/FirstPageController.java
@@ -2,7 +2,13 @@ package com.example.onlineshop_site2.controllers;
 
 import com.example.onlineshop_site2.models.dtos.requests.FirstPageReq;
 import com.example.onlineshop_site2.models.dtos.responses.FirstPageItemRes;
+import com.example.onlineshop_site2.models.dtos.responses.FiveCategoryItemRes;
 import com.example.onlineshop_site2.services.service.FirstPageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +25,12 @@ import org.springframework.web.multipart.MultipartFile;
 public class FirstPageController {
 
     private final FirstPageService firstPageService;
+    @Operation(summary = "Получить первую страницу")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = FirstPageItemRes.class))})
+    })
     @GetMapping("/firstPage")
     public ResponseEntity<FirstPageItemRes> getItem(){
         FirstPageItemRes res = firstPageService.getItem();
@@ -27,6 +39,12 @@ public class FirstPageController {
                 .body(res);
     }
 
+    @Operation(summary = "Обновить первую страницу")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Application found",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = FirstPageItemRes.class))})
+    })
     @PutMapping("/firstPage")
     public ResponseEntity<FirstPageItemRes> updateItem(@RequestBody FirstPageReq req){
         FirstPageItemRes res = firstPageService.updateItem(req);
@@ -35,6 +53,10 @@ public class FirstPageController {
                 .body(res);
     }
 
+    @Operation(summary = "Обновить фото первой страницы")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200")
+    })
     @PostMapping("/firstPage/photo")
     public ResponseEntity<?> updatePhoto(@RequestParam(value = "file", required = true) MultipartFile file){
         firstPageService.updatePhoto(file);
