b0f51a3056aadf26ef268a0b0089c46881fa8132
TheKelp1e <vova_krasnov_2004@mail.ru>
2023-12-12 21:45:31 +0300

    save

--- src/main/java/com/example/onlineshop_site2/models/entities/FirstPage.java
+++ src/main/java/com/example/onlineshop_site2/models/entities/FirstPage.java
@@ -23,11 +23,11 @@ public class FirstPage {
 
     private String description;
 
-    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, orphanRemoval = true)
+    @OneToOne
     @JoinColumn(name = "good_id")
     private Good good;
 
-    @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH}, orphanRemoval = true)
+    @OneToOne
     @JoinColumn(name = "photo_id")
     private Photo photo;
 }
