7b87609f4edaa4a08e69c078ae4309f43b7eb449
Sh1bari <vova_krasnov_2004@mail.ru>
2023-12-12 14:52:37 +0300

    new

--- src/main/java/com/example/onlineshop_site2/models/entities/Good.java
+++ src/main/java/com/example/onlineshop_site2/models/entities/Good.java
@@ -19,7 +19,7 @@ import java.util.Set;
 @Table(name = "goods")
 public class Good {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
